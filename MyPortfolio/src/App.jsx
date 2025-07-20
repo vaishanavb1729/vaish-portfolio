bfd24dd99c6fd68a2e6ef6e72ed5ecd6ba7619f4
Vaishanav Bhosale <vaishanavb1729@gmail.com>
2025-07-20 15:21:20 +0530

    Project setup is done

---
+++ MyPortfolio/src/App.jsx
@@ -0,0 +1,23 @@
+import { useState } from 'react'
+import Navbar from './Components/Navbar.jsx';
+import Hero from './Components/Hero.jsx';
+import Projects from './Components/Project.jsx';
+import Contacts from './Components/Contact.jsx';
+import './App.css'
+
+function App() {
+  
+
+  return 
+    <>
+    <Navbar/>
+    <Hero/>
+    <Projects/>
+    <Contacts/>
+
+
+    </>
+  
+}
+
+export default App
