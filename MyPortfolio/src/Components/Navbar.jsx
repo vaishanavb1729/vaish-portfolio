89bda1559f1ffaffe9ca2b131c306fbbd6753f4a
Vaishanav Bhosale <vaishanavb1729@gmail.com>
2025-07-20 18:12:33 +0530

    Animation effect is applied

--- MyPortfolio/src/Components/Navbar.jsx
+++ MyPortfolio/src/Components/Navbar.jsx
@@ -1,4 +1,8 @@
-function Navbar(){
+import {motion} from "framer-motion";
+export  const Navbar=()=>{
 
+    return<motion.nav className="navbar" initial={{y:-100}} animate={{y:0}}transition={{duration:0.6,ease:"easeOut"}}>
+    <motion.div className="logo" whileHover={{scale:1.05}} whileTap={{scale:0.95}}>Portfolio</motion.div>
+    </motion.nav>
 }
-export default NavBar;
+export default Navbar;
